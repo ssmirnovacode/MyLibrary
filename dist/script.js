d854938b8197c2c6c839a16cba83790803812e3a
ssmirnova <scorpio213@mail.ru>
2020-12-15 17:05:23 +0100

    Carousel my version not working

--- dist/script.js
+++ dist/script.js
@@ -132,9 +132,56 @@ __webpack_require__.r(__webpack_exports__);
 
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
   for (let i = 0; i < this.length; i++) {
-    const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width; //we get width from computed width of the wrapper
+    const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width,
+          //we get width from computed width of the wrapper
+    slides = this[i].querySelectorAll('.carousel-item'),
+          btnPrev = this[i].querySelector('.carousel-prev'),
+          btnNext = this[i].querySelector('.carousel-next'),
+          dots = this[i].querySelectorAll('.carousel-indicators li');
+    let slideIndex = 0;
 
-    const slides = this[i].querySelectorAll('.carousel-item');
+    function hideSlides() {
+      slides.forEach(slide => {
+        slide.style.width = '0px';
+      });
+    }
+
+    hideSlides();
+    slides[slideIndex].style.width = width;
+    dots.forEach((item, i) => {
+      item.click(() => {
+        hideSlides();
+        slides[i].style.width = width;
+      });
+    });
+    btnPrev.click(e => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (slideIndex > 0) {
+        slideIndex--;
+        hideSlides();
+        slides[slideIndex].style.width = width;
+      } else {
+        slideIndex = slides.length - 1;
+        hideSlides();
+        slides[slideIndex].style.width = width;
+      }
+    });
+    btnNext.click(e => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (slideIndex < slides.length - 1) {
+        slideIndex++;
+        hideSlides();
+        slides[slideIndex].style.width = width;
+      } else {
+        slideIndex = 0;
+        hideSlides();
+        slides[slideIndex].style.width = width;
+      }
+    });
   }
 };
 
--- src/js/lib/components/carousel.js
+++ src/js/lib/components/carousel.js
@@ -2,10 +2,59 @@ import $ from '../core';
 
 $.prototype.carousel = function() {
     for (let i=0; i < this.length; i++) {
-        const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width; //we get width from computed width of the wrapper
-        const slides = this[i].querySelectorAll('.carousel-item');
+        const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width, //we get width from computed width of the wrapper
+            slides = this[i].querySelectorAll('.carousel-item'),
+            btnPrev = this[i].querySelector('.carousel-prev'),
+            btnNext = this[i].querySelector('.carousel-next'),
+            dots = this[i].querySelectorAll('.carousel-indicators li');
 
+        let slideIndex = 0;
+
+        function hideSlides() {
+            slides.forEach(slide => {
+                slide.style.width = '0px';
+            });
+        }
+        hideSlides();
         
+        slides[slideIndex].style.width = width;
+
+        dots.forEach((item,i) => {
+            item.click( () => {
+                hideSlides();
+                slides[i].style.width = width;
+            });
+        });
+
+        btnPrev.click( (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (slideIndex > 0) {
+                slideIndex--;
+                hideSlides();
+                slides[slideIndex].style.width = width;
+            }
+            else {
+                slideIndex = slides.length-1;
+                hideSlides();
+                slides[slideIndex].style.width = width;
+            }
+        })
+
+        btnNext.click( (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (slideIndex < slides.length-1) {
+                slideIndex++;
+                hideSlides();
+                slides[slideIndex].style.width = width;
+            }
+            else {
+                slideIndex = 0;
+                hideSlides();
+                slides[slideIndex].style.width = width;
+            }
+        })
     }
 };
 
